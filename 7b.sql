d37530b8973ac11975f0fc19dce2ae4e06ccdbc6
ewelina452 <ewelinabrach452@gmail.com>
2021-05-11 18:14:48 +0200

    Update 7b.sql

--- 7b.sql
+++ 7b.sql
@@ -1,37 +1,38 @@
+create database fibo;
+use fibo;
 
-
-
---CREATE FUNCTION Fibo(@m INT)
---RETURNS @tab TABLE(ciag_fibonacciego int)
---as
---begin
-declare @n int=0,
+CREATE FUNCTION Fibo(@n INT)
+RETURNS @tab TABLE(liczba int, nr int)
+as
+begin
+declare 
+@liczba int =0,
  @fib0 int =0,
  @fib1 int =1,
  @i int=2
-declare @tab table (liczba int)
-insert into @tab values (@fib0)
 
-while (@i<= 47)
+--declare @tab table (liczba int)
+insert into @tab values (@fib0, @i-2)
+
+while (@i<= @n)
 begin
 
-set @n=@fib0+@fib1
+set @liczba=@fib0+@fib1
 set @fib1=@fib0
-set @fib0=@n
+set @fib0=@liczba
 set @i+=1
-insert into @tab values (@fib0)
+insert into @tab values (@fib0,@i-2)
 end
---return 
---end;
+return 
+end;
 
 
---CREATE OR ALTER PROCEDURE fibo(@x INT)
---AS
---BEGIN
---SELECT * FROM dbo.Fibo(@x)
---END;
+CREATE OR ALTER PROCEDURE fib(@n INT)
+AS
+BEGIN
+SELECT * FROM dbo.Fibo(@n)
+END;
 
 
---EXEC fibo 15;
+EXEC fib 20;
 
-select*from @tab
